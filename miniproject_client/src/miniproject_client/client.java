c07d20d09f8c6795c59626e0499f3b326f2feb6d
DitteFabrin <ditte.brodersen@live.dk>
2018-10-16 13:28:13 +0200

    Made it go further after you type yes/no if you want to start game

--- miniproject_client/src/miniproject_client/client.java
+++ miniproject_client/src/miniproject_client/client.java
@@ -21,7 +21,7 @@ public class client {
 			try {
 				DataOutputStream toServer = null;
 				DataInputStream fromServer = null;
-
+				
 				// Create a socket to connect to the server
 				Socket serverSocket = new Socket("localhost", 2000); // This shouldn't be local host but the serve
 																		// machine�s host name or IP address
@@ -34,16 +34,17 @@ public class client {
 				toServer = new DataOutputStream(serverSocket.getOutputStream());
 
 				// Enter if you want to start the game
-				System.out.print("Do you want to start a game of rock, paper scissors? Type yes or no: ");
+				System.out.println("Do you want to start a game of rock, paper scissors? Type yes or no: ");
 				String wantToStartGame = input.next();
+				
 
 				// Send to server 'yes' or 'no'
 				toServer.writeBytes(wantToStartGame);
 				// toServer.flush();
 
 				// If user types yes it continues if no it stops
-				String answerStartGame = input.next();
-				if (answerStartGame.equalsIgnoreCase("no")) {
+				
+				if (wantToStartGame.equalsIgnoreCase("no")) {
 					keepRunning = false;
 				}
 				// ----------------------------GAME-------------------------------
